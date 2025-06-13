81447e7bdbc345538c549d5dfd76432956f6972e
SAKSHAM <sakshrwt@gmail.com>
2025-06-13 18:26:30 +0530

    refactor: Changed the way fonts were imported

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,20 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
-        {/* font-sans */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-          rel="stylesheet"
-        />
-        {/* fon-mono */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="selection:text-white selection:bg-neutral-900">
         <Navbar />
         <main>{children}</main>
